107ca037e17623788070aa6ba9f595e5b5bc45a2
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-07 00:30:33 -0500

    added innerHTML in then

--- assets/javascript/homepage.js
+++ assets/javascript/homepage.js
@@ -4,10 +4,18 @@ var name = document.querySelector('.name');
 var desc = document.querySelector('desc');
 var temp = document.querySelector('.temp');
 
-button.addEventListener('click', function() {
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=403f69cea330b6713e726d6736d05afc')
-.then(response => response.json())
-.then(data => console.log(data))
+button.addEventListener('click', function () {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=403f69cea330b6713e726d6736d05afc')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .then(data => {
+            var nameValue = data['name'];
+            var tempValue = data['main']['temp'];
+            var descValue = data['weather'][0]['description'];
 
-.catch(err => alert("Could not locate that city!"))
+            name.innerHTML = nameValue;
+            temp.innerHTML = tempValue;
+            desc.innerHTML = descValue;
+        })
+        .catch(err => alert("Could not locate that city!"))
 });
