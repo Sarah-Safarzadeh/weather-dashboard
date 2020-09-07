5e183771fa8ccf7c47de37b81ce6eaefae970ce1
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-06 23:54:45 -0500

    added fetch

--- assets/javascript/homepage.js
+++ assets/javascript/homepage.js
@@ -3,3 +3,5 @@ var inputValue = document.querySelector('.inputValue');
 var name = document.querySelector('.name');
 var desc = document.querySelector('desc');
 var temp = document.querySelector('.temp');
+
+fetch('api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=403f69cea330b6713e726d6736d05afc');
